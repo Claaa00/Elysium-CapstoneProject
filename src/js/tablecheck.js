db6c5405f0d82537db8dcfd27a2f44523a77c23b
Claaa00 <cmartinp0804@gmail.com>
2023-01-18 13:39:37 +0800

    checkbox function update

--- src/js/tablecheck.js
+++ src/js/tablecheck.js
@@ -1,16 +1,84 @@
 $(document).ready(function () {
-  $("table").simpleCheckboxTable();
+  $(".all-table").simpleCheckboxTable();
 });
 
 $(document).ready(function () {
-  $("table").simpleCheckboxTable({
+  $(".all-table").simpleCheckboxTable({
     onCheckedStateChanged: function ($checkbox) {
       // Do something
       // $(".checkbox").click(function () {
       //   $("#archive-button").prop("disabled", !$(".checkbox:checked").length);
       // });
 
-      $("#archive-button").prop("disabled", !$(".checkbox:checked").length);
+      $(".all-btn").prop("disabled", !$(".checkbox:checked").length);
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".apt-table").simpleCheckboxTable();
+});
+
+$(document).ready(function () {
+  $(".apt-table").simpleCheckboxTable({
+    onCheckedStateChanged: function ($checkbox) {
+      // Do something
+      // $(".checkbox").click(function () {
+      //   $("#archive-button").prop("disabled", !$(".checkbox:checked").length);
+      // });
+
+      $(".apt-btn").prop("disabled", !$(".checkbox:checked").length);
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".lwn-table").simpleCheckboxTable();
+});
+
+$(document).ready(function () {
+  $(".lwn-table").simpleCheckboxTable({
+    onCheckedStateChanged: function ($checkbox) {
+      // Do something
+      // $(".checkbox").click(function () {
+      //   $("#archive-button").prop("disabled", !$(".checkbox:checked").length);
+      // });
+
+      $(".lwn-btn").prop("disabled", !$(".checkbox:checked").length);
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".bv-table").simpleCheckboxTable();
+});
+
+$(document).ready(function () {
+  $(".bv-table").simpleCheckboxTable({
+    onCheckedStateChanged: function ($checkbox) {
+      // Do something
+      // $(".checkbox").click(function () {
+      //   $("#archive-button").prop("disabled", !$(".checkbox:checked").length);
+      // });
+
+      $(".bv-btn").prop("disabled", !$(".checkbox:checked").length);
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".arc-table").simpleCheckboxTable();
+});
+
+$(document).ready(function () {
+  $(".arc-table").simpleCheckboxTable({
+    onCheckedStateChanged: function ($checkbox) {
+      // Do something
+      // $(".checkbox").click(function () {
+      //   $("#archive-button").prop("disabled", !$(".checkbox:checked").length);
+      // });
+
+      $(".arc-btn").prop("disabled", !$(".checkbox:checked").length);
     },
   });
 });
